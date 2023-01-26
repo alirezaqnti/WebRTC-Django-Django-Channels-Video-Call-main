7f350de6478d313fd3f343c41958422729e30d62
alirezaqnti <64876382+alirezaqnti@users.noreply.github.com>
2023-01-26 11:45:56 +0330

    00

--- call/views.py
+++ call/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+import logging
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    logging.debug("Oh hai!")
+    return render(request, "index.html")
