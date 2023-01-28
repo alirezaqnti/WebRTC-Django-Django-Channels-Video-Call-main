9c8c0da922a81d41bf68a76ef2083f9570e5f4a4
alirezaqnti <64876382+alirezaqnti@users.noreply.github.com>
2023-01-28 15:56:02 +0330

    00

--- static/call.js
+++ static/call.js
@@ -65,19 +65,21 @@ let sdpConstraints = {
 
 let socket;
 var callSocket;
-let ws_scheme = window.location.protocol == "https:" ? "wss://" : "ws://";
-callSocket = new WebSocket(ws_scheme + window.location.host + "/ws/call/");
-callSocket.onopen = async function (event) {
-  //let's send myName to the socket
-  callSocket.send(
-    JSON.stringify({
-      type: "login",
-      data: {
-        name: myName,
-      },
-    })
-  );
-};
+function connectSocket(e) {
+  let ws_scheme = window.location.protocol == "https:" ? "wss://" : "ws://";
+  callSocket = new WebSocket(ws_scheme + window.location.host + "/ws/call/");
+  callSocket.onopen = async function (event) {
+    //let's send myName to the socket
+    callSocket.send(
+      JSON.stringify({
+        type: "login",
+        data: {
+          name: myName,
+        },
+      })
+    );
+  };
+}
 
 callSocket.onmessage = async function (e) {
   let response = JSON.parse(e.data);
